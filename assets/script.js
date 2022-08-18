b5b9c45ad92cc42d7602a90a0d6b57849584ca9c
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-08-18 12:25:30 -0600

    deleted functions to create a future streamlined if statement

--- assets/script.js
+++ assets/script.js
@@ -6,31 +6,16 @@ var numbersGen = [...Array(10)].map((_, i) => i);
 console.log(numbersGen)
 var alphabetGen = [...Array(26)].map((_, i) => String.fromCharCode(i + 97))
 console.log(alphabetGen)
-
-// Functions
-function randSpecialChar () {
-  specialChar[Math.floor(Math.random() * specialChar.length)];
-}
-function randNumbers () {
-  var numbers = numbersGen();
-  numbers[Math.floor(Math.random() * numbers.length)];
-}
-
-function randLower () {
-  var lowercase = alphabetGen ();
-  lowercase[Math.floor(Math.random() * lowercase.length)];
-}
-
-function randUpper () {
-  var uppercase = alphabetGen ().toUpperCase ();
-  uppercase[Math.floor(Math.random() * uppercase.length)];
-}
-
+var megaArray = []
+// User Function
 var userLower = confirm("Would you like lowercase letters? \nOk for yes, Cancel for no");
+ 
 var userCapital = confirm("Would you like CAPITAL letters? \nOk for yes, Cancel for no");
-var userNumber = confirm("Would you like numbers? \nOk for yes, Cancel for no")
-var userSpecial = confirm("Would you like special characters? \nOk for yes, Cancel for no");
+  
+var userNumber = confirm("Would you like numbers? \nOk for yes, Cancel for no");
 
+var userSpecial = confirm("Would you like special characters? \nOk for yes, Cancel for no");
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
