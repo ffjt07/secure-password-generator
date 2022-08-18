99c903a3808ddc1113dddf9f0ca09d8d7446aa50
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-08-18 16:10:24 -0600

    adjusted spacing and added comments in function

--- assets/script.js
+++ assets/script.js
@@ -9,39 +9,40 @@ console.log(alphabetGen)
 var megaArray = []
 // User Function
 function generatePassword () {
-var userLower = confirm("Would you like lowercase letters? \nOk for yes, Cancel for no");
-var userCapital = confirm("Would you like CAPITAL letters? \nOk for yes, Cancel for no");
-var userNumber = confirm("Would you like numbers? \nOk for yes, Cancel for no");
-var userSpecial = confirm("Would you like special characters? \nOk for yes, Cancel for no");
 
-if (!userLower && !userCapital && !userNumber && !userSpecial) {
-  alert("You need to select one of the password elements");
-  return generatePassword();
-}
+  var userLower = confirm("Would you like lowercase letters? \nOk for yes, Cancel for no");
+  var userCapital = confirm("Would you like CAPITAL letters? \nOk for yes, Cancel for no");
+  var userNumber = confirm("Would you like numbers? \nOk for yes, Cancel for no");
+  var userSpecial = confirm("Would you like special characters? \nOk for yes, Cancel for no");
 
-if (userLower) {
-  for (var i = 0; i < alphabetGen.length; i++){
-    megaArray.push(alphabetGen[i]);
-  } 
-}
+  if (!userLower && !userCapital && !userNumber && !userSpecial) {
+    alert("You need to select one of the password elements");
+    return generatePassword();
+  }
 
-if (userCapital) {
-  for (var i = 0; i < alphabetGen.length; i++){
-    megaArray.push(alphabetGen[i].toUpperCase());
-  } 
-}
+  if (userLower) {
+    for (var i = 0; i < alphabetGen.length; i++){
+      megaArray.push(alphabetGen[i]);
+    } 
+  }
 
-if (userNumber) {
-  for (var i = 0; i < numbersGen.length; i++){
-    megaArray.push(numbersGen[i]);
-  } 
-}
+  if (userCapital) {
+    for (var i = 0; i < alphabetGen.length; i++){
+      megaArray.push(alphabetGen[i].toUpperCase());
+    } 
+  }
 
-if (userSpecial) {
-  for (var i = 0; i < specialChar.length; i++){
-    megaArray.push(specialChar[i]);
-  } 
-}
+  if (userNumber) {
+    for (var i = 0; i < numbersGen.length; i++){
+      megaArray.push(numbersGen[i]);
+    } 
+  }
+
+  if (userSpecial) {
+    for (var i = 0; i < specialChar.length; i++){
+      megaArray.push(specialChar[i]);
+    } 
+  }
 
 }
 // Get references to the #generate element
