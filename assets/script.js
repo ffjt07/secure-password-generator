62f24a5b85b660c5a23aa2a1a08c4acc1515aaef
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-08-17 23:24:00 -0600

    added random functions for password criteria and added numbers prompt

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,34 @@
 // Global Variables
 var specialChar = ["!", "\u0022", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\u005B", "\u005C", "\u005D", "^", "_", "`", "{", "|", "}", "~"]
 console.log(specialChar)
-var numbers = [...Array(10)].map((_, i) => i);
-console.log(numbers)
-var alphabet = [...Array(26)].map((_, i) => String.fromCharCode(i + 97))
-console.log(alphabet)
+var numbersGen = [...Array(10)].map((_, i) => i);
+console.log(numbersGen)
+var alphabetGen = [...Array(26)].map((_, i) => String.fromCharCode(i + 97))
+console.log(alphabetGen)
 
-var userSpecial = confirm("Would you like special characters? \nOk for yes, Cancel for no")
-var userCapital = confirm("Would you like CAPITAL letters? \nOk for yes, Cancel for no")
-var userLower = confirm("Would you like lowercase letters? \nOk for yes, Cancel for no")
+// Functions
+function randSpecialChar () {
+  specialChar[Math.floor(Math.random() * specialChar.length)];
+}
+function randNumbers () {
+  var numbers = numbersGen();
+  numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function randLower () {
+  var lowercase = alphabetGen ();
+  lowercase[Math.floor(Math.random() * lowercase.length)];
+}
+
+function randUpper () {
+  var uppercase = alphabetGen ().toUpperCase ();
+  uppercase[Math.floor(Math.random() * uppercase.length)];
+}
+
+var userLower = confirm("Would you like lowercase letters? \nOk for yes, Cancel for no");
+var userCapital = confirm("Would you like CAPITAL letters? \nOk for yes, Cancel for no");
+var userNumber = confirm("Would you like numbers? \nOk for yes, Cancel for no")
+var userSpecial = confirm("Would you like special characters? \nOk for yes, Cancel for no");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
