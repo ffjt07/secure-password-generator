6c632dd9e1f662d6ffd7b81a89812eb8c7e4855f
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-08-19 11:27:54 -0600

    updated passwordGen to function and changed output to string to display in text area

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,66 @@
 // Assignment code here
 // Global Variables
-var specialChar = ["!", "\u0022", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\u005B", "\u005C", "\u005D", "^", "_", "`", "{", "|", "}", "~"];
-console.log(specialChar)
+var specialChar = [
+  "!",
+  "\u0022",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "\u005B",
+  "\u005C",
+  "\u005D",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+console.log(specialChar);
 var numbersGen = "0123456789".split("");
-console.log(numbersGen)
+console.log(numbersGen);
 var alphabetGen = "abcdefghijklmnopqrstuvwxyz".split("");
-console.log(alphabetGen)
-var megaArray = []
+console.log(alphabetGen);
+var megaArray = [];
 // User Function
-function generatePassword () {
-  var userLength = prompt("How many characters do you want your password? \n Must be between 8 and 128 characters.")
-  var userLower = confirm("Would you like lowercase letters? \nOk for yes, Cancel for no");
-  var userCapital = confirm("Would you like CAPITAL letters? \nOk for yes, Cancel for no");
-  var userNumber = confirm("Would you like numbers? \nOk for yes, Cancel for no");
-  var userSpecial = confirm("Would you like special characters? \nOk for yes, Cancel for no");
+function generatePassword() {
+  var userLength = prompt(
+    "How many characters do you want your password? \n Must be between 8 and 128 characters."
+  );
+  var userLower = confirm(
+    "Would you like lowercase letters? \nOk for yes, Cancel for no"
+  );
+  var userCapital = confirm(
+    "Would you like CAPITAL letters? \nOk for yes, Cancel for no"
+  );
+  var userNumber = confirm(
+    "Would you like numbers? \nOk for yes, Cancel for no"
+  );
+  var userSpecial = confirm(
+    "Would you like special characters? \nOk for yes, Cancel for no"
+  );
 
   if (userLength < 8 || userLength > 128) {
-    alert ("Please select a number between 8 and 128 \nfor password length.");
-    return generatePassword ();
+    alert("Please select a number between 8 and 128 \nfor password length.");
+    return generatePassword();
   }
   if (!userLower && !userCapital && !userNumber && !userSpecial) {
     alert("You need to select one of the password elements");
@@ -25,38 +68,44 @@ function generatePassword () {
   }
 
   if (userLower) {
-    for (var i = 0; i < alphabetGen.length; i++){
+    for (var i = 0; i < alphabetGen.length; i++) {
       megaArray.push(alphabetGen[i]);
-    } 
+    }
   }
 
   if (userCapital) {
-    for (var i = 0; i < alphabetGen.length; i++){
+    for (var i = 0; i < alphabetGen.length; i++) {
       megaArray.push(alphabetGen[i].toUpperCase());
-    } 
+    }
   }
 
   if (userNumber) {
-    for (var i = 0; i < numbersGen.length; i++){
+    for (var i = 0; i < numbersGen.length; i++) {
       megaArray.push(numbersGen[i]);
-    } 
+    }
   }
 
   if (userSpecial) {
-    for (var i = 0; i < specialChar.length; i++){
+    for (var i = 0; i < specialChar.length; i++) {
       megaArray.push(specialChar[i]);
-    } 
+    }
   }
-  console.log (megaArray)
-  var password = []
-  var passwordGen = megaArray[Math.floor(Math.random() * megaArray.length)];
-  if(password.length < userLength){
-    for(var i = 0; i < userLength; i++){
-      password.push(passwordGen);
+  console.log(megaArray);
+  var password = [];
+  function passwordGen() {
+    var randChar = megaArray[Math.floor(Math.random() * megaArray.length)];
+    password.push(randChar);
+  }
+  if (password.length < userLength) {
+    for (i = 0; i < userLength; i++) {
+      passwordGen();
     }
   }
   console.log(password);
+  return password.join("");
 }
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -66,8 +115,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
